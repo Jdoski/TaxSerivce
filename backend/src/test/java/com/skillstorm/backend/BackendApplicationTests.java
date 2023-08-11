9eb7b2f52262a94c1a0d3d7f8c4699a9a52cb9f4
cris-me <melendez.cristopher@live.com>
2023-08-11 04:51:56 -0400

    testing

--- backend/src/test/java/com/skillstorm/backend/BackendApplicationTests.java
+++ backend/src/test/java/com/skillstorm/backend/BackendApplicationTests.java
@@ -1,13 +1,37 @@
 package com.skillstorm.backend;
+import com.skillstorm.backend.services.TaxReturnService;
+import com.skillstorm.backend.services.UserService;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class BackendApplicationTests {
 
+	@Autowired
+	TaxReturnService taxReturnService;
+
+	@Autowired
+	UserService userService;
+
+	String filing_status = "widow";
+	String username = "thursday@gmail.com";
+	String password = "test";
+
 	@Test
-	void contextLoads() {
+	void testDeduction() {
+		assertEquals(taxReturnService.deduction(filing_status), 27700.01, 1);
 	}
+
+	@Test
+	void testLogin(){
+		assertTrue(userService.checkLogin(username, password));
+	}
+
+
 
 }
