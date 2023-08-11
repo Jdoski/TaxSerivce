2fe6aa6e4236e4fd1ed66868f2ad12c950186399
cris-me <melendez.cristopher@live.com>
2023-08-11 12:10:11 -0400

    Final Update

--- backend/src/main/java/com/skillstorm/backend/config/DocumentDBConf.java
+++ backend/src/main/java/com/skillstorm/backend/config/DocumentDBConf.java
@@ -1,7 +1,5 @@
 package com.skillstorm.backend.config;
 
-import org.omg.CORBA.Environment;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.mongo.MongoProperties;
 import org.springframework.boot.autoconfigure.mongo.MongoPropertiesClientSettingsBuilderCustomizer;
@@ -15,10 +13,8 @@ public class DocumentDBConf {
 
     private MongoProperties properties;
 
-    @Value("${TRUST_STORE}")
     public static String KEY_STORE_TYPE = "/tmp/certs/rds-truststore.jks";
 
-    @Value("${TRUST_STORE_PS}")
     public static String DEFAULT_KEY_STORE_PASSWORD = "password";
 
     public DocumentDBConf(final MongoProperties properties) {
